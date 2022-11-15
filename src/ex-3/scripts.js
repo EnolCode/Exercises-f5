5a986e9233c78ab41e782db992a19c8552202779
Enol-Igareta <enol1code@gmail.com>
2022-11-15 21:17:43 +0100

    añadiendo ejercicios

---
+++ src/ex-3/scripts.js
@@ -0,0 +1,18 @@
+function saludarUsuario() {
+  //   let input = document.getElementById("input").value;
+  //   let div = document.getElementById("div");
+  //   let form = document.getElementById("form");
+  //   let button = document.getElementById("button");
+  //   button.addEventListener("click", () => {
+  //     button.preventDefault();
+  //     div.textContent = input;
+  //   });
+  //   console.log(input);
+}
+
+let nombre = document.getElementById("input");
+const div = getElementById("div");
+div.innerHTML = `Hola`;
+console.log(div);
+console.log(nombre);
+// saludarUsuario();
