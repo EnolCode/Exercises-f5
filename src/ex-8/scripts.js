32a7c7b93d66ee31eba7bc10d3b5eeee1ad01bc8
Enol-Igareta <enol1code@gmail.com>
2022-11-22 22:21:47 +0100

    exercise 8 done

--- src/ex-8/scripts.js
+++ src/ex-8/scripts.js
@@ -1,4 +1,4 @@
-// Escribe un programa que pida una frase y escriba cuantas veces aparece una letra
+// Escribe un programa que pida una frase y escriba las vocales que aparecen
 
 const button = document.querySelector(".button");
 
@@ -6,18 +6,21 @@ function converStringToArray(str) {
   return str.split("");
 }
 
-function searchLetter(text, lett) {
+function searchLetter(text) {
   let count = 0;
   text.forEach((el) => {
-    if (el === lett) count++;
+    if (el === "a") count++;
+    if (el === "e") count++;
+    if (el === "i") count++;
+    if (el === "o") count++;
+    if (el === "u") count++;
   });
   return count;
 }
 
 button.addEventListener("click", () => {
   const text = document.querySelector(".text").value;
-  const letter = document.querySelector(".letter").value;
   const divResult = document.querySelector(".result");
   const str = converStringToArray(text);
-  divResult.innerHTML = searchLetter(str, letter);
+  divResult.innerHTML = searchLetter(str);
 });
