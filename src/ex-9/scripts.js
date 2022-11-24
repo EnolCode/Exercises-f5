8191d51b2afb291506cddaf5d71bfd76dbf400d9
Enol-Igareta <enol1code@gmail.com>
2022-11-24 20:35:00 +0100

    exercise 9 done

---
+++ src/ex-9/scripts.js
@@ -0,0 +1,25 @@
+// <!-- Escribe un programa que pida una frase y escriba las vocales que aparecen -->
+
+const button = document.querySelector(".button");
+
+function searchVowels(str, regex) {
+  const newStr = str.match(regex);
+  return newStr;
+}
+
+function deleteVowelsDuplicates(arr) {
+  const dataArr = new Set(arr);
+  let result = [...dataArr];
+  return result;
+}
+
+function printVowelsResult(element, result) {
+  return (element.innerHTML = `Las vocales que aparecen en tu frase son ${result}`);
+}
+
+button.addEventListener("click", () => {
+  const regex1 = /[aeiou]/g;
+  const text = document.querySelector(".text").value;
+  let result = deleteVowelsDuplicates(searchVowels(text, regex1));
+  printVowelsResult(document.querySelector(".result"), result);
+});
